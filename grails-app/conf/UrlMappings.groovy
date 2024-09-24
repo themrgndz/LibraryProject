ed26d6126a3932fc643b06425adc5e7b5f2d4012
themrgndz <emrgndz_pc@outlook.com>
2024-09-24 14:22:27 +0300

    24.09.2024

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,8 +1,27 @@
-
 class UrlMappings {
     static mappings = {
-        "/"(controller: "anasayfa", action: "index")
-        "500"(view:'/error')
-        "404"(view:'/notFound')
+        "/$controller/$action?/$id?"{
+            constraints {
+
+            }
+        }
+
+        // Anasayfa için yönlendirme
+        "/"(controller: "anasayfa", action: "anasayfa")
+
+        // Kitaplar listesi için yönlendirme
+        "/books"(controller: "book", action: "list")
+
+        // Kitap detayları için yönlendirme
+        "/books/$id"(controller: "book", action: "show")
+
+        // Admin paneli için yönlendirme
+        "/admin"(controller: "admin", action: "dashboard")
+
+        // 404 Hata Sayfası
+        "404"(view: '/notFound')
+
+        // 500 Hata Sayfası
+        "500"(view: '/error')
     }
 }
---
+++ grails-app/controllers/anasayfaController.groovy
@@ -0,0 +1,8 @@
+class anasayfaController{
+    def anasayfa(){
+        render(view: 'anasayfa')
+    }
+    def giris(){
+        render(view: 'giris')
+    }
+}
