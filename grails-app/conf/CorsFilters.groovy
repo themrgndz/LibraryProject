c912eee4a05d4598c18bac61acb25bbafab4a781
themrgndz <emrgndz_pc@outlook.com>
2024-10-17 08:38:51 +0300

    17.10.2024 08.38

---
+++ grails-app/conf/CorsFilters.groovy
@@ -0,0 +1,29 @@
+package mylibrary
+
+import javax.servlet.*
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class CorsFilters {
+
+    def filters = {
+        all(uri: "/**") { // Tüm URI'lar için filtre uygula
+            before = {
+                if (request instanceof HttpServletRequest) {
+                    HttpServletResponse response = (HttpServletResponse) response
+
+                    response.setHeader("Access-Control-Allow-Origin", "*")
+                    response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+                    response.setHeader("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+                    response.setHeader("Access-Control-Allow-Credentials", "true")
+
+                    if (request.method == "OPTIONS") {
+                        response.status = 200
+                        return false
+                    }
+                }
+                return true
+            }
+        }
+    }
+}
--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -7,6 +7,10 @@ class UrlMappings {
         "/list"(controller: 'home', action: 'list')
         "/list/search"(controller: 'home', action: 'search')
 
+        // List
+        "/api/list"(controller: 'home', action: 'list')
+        "/api/save"(controller: 'home', action: 'save')
+
         // Save
         "/save"(controller: 'home', action: 'save')
 
@@ -18,5 +22,9 @@ class UrlMappings {
 
         // Error
         "500"(view:'/error')
+
+        // API Routes
+        "/api/list/$id"(controller: 'book', action: [GET: 'show', PUT: 'update', DELETE: 'delete']) // ID'ye göre kitap işlemleri
+
     }
 }
